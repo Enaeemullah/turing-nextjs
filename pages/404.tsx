3fdc9dfe130f50abd3a4e6f1f79cb75c730f411b
Naeem Ullah <enaeemullah@gmail.com>
2022-02-22 11:36:07 +0500

    update 404 Page!

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+import { Button } from 'react-bootstrap';
 
 function custom404() {
-  return <div>This Page is not found.</div>;
+  const router = useRouter();
+  const homeHandler = (e: any) => {
+    e.preventDefault();
+    router.push('/');
+  };
+  return (
+    <>
+      This Page is not found.
+      <Button className='btn btn-primary' onClick={homeHandler}>
+        Go Back.
+      </Button>
+      ;
+    </>
+  );
 }
 
 export default custom404;
--- pages/SignIn/index.tsx
+++ pages/SignIn/index.tsx
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import styles from '../../styles/Home.module.css';
 import SignIn from '../../components/Signin';
-  
+
 export default function login() {
   return (
     <>
